5f00595374f8612ceea70bf4e7bf5e73b3c9f8da
Sami Uddin <158276750+SamiUddin95@users.noreply.github.com>
2024-06-30 14:33:04 +0500

    test-backend-mohsin-bhai

--- Backend/Models/ERPContext.cs
+++ Backend/Models/ERPContext.cs
@@ -50,7 +50,7 @@ namespace Backend.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Data Source=TEK;Initial Catalog=ERP;user id=sa1;password=123qwe;integrated security=True;Trusted_Connection=True;Encrypt=Yes;TrustServerCertificate=Yes");
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-2TI6E4O\SQLEXPRESS;Initial Catalog=SMART_ERP;user id=sa;password=11;integrated security=True;Trusted_Connection=True;Encrypt=Yes;TrustServerCertificate=Yes");
             }
         }
 
