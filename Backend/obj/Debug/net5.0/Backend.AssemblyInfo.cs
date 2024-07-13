af72cb616ae3e3e8b70ef3d8db8ef683d2014201
Sami Uddin <158276750+SamiUddin95@users.noreply.github.com>
2024-07-13 20:57:07 +0500

    done

--- Backend/obj/Debug/net5.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net5.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651c702c00c457bbabe9c5fa42bfc18ef6bcadac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
