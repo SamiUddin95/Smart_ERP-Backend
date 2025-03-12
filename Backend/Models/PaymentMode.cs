08c93fecbe8e4efc979ba0ea96ef03a33ada225a
abdullah-hasham-code <76914757+abdullah-hasham-code@users.noreply.github.com>
2025-03-12 12:38:29 +0500

    done

--- Backend/Models/PaymentMode.cs
+++ Backend/Models/PaymentMode.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Models
 {
-    [Table("Payment_Mode")]
     public partial class PaymentMode
     {
         public PaymentMode()
@@ -14,21 +11,12 @@ namespace Backend.Models
         }
 
         public int Id { get; set; }
-        [Column("Payment_mode")]
-        [StringLength(50)]
         public string PaymentMode1 { get; set; }
-        [Column("CREATED_AT", TypeName = "datetime")]
         public DateTime CreatedAt { get; set; }
-        [Column("UPDATED_AT", TypeName = "datetime")]
         public DateTime? UpdatedAt { get; set; }
-        [Column("CREATED_BY")]
-        [StringLength(255)]
         public string CreatedBy { get; set; }
-        [Column("UPDATED_BY")]
-        [StringLength(255)]
         public string UpdatedBy { get; set; }
 
-        [InverseProperty("ModeNavigation")]
         public ICollection<ReceivedPayments> ReceivedPayments { get; set; }
     }
 }
