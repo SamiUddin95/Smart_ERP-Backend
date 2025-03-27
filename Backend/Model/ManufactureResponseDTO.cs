3d15bbf1f08ff6bda040e91f2de34579f6157607
Sami <samiuddin12318@gmail.com>
2025-03-27 15:18:44 +0500

    image work

---
+++ Backend/Model/ManufactureResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Backend.Model
+{
+    public class ManufactureResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telephoneno { get; set; }
+        public string Telephoneno2 { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public long? Sno { get; set; }
+        public string Picture { get; set; }
+    }
+}
